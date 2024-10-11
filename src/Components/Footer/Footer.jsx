da4c11e8001f4d0f3e1bc66b0bb4a85ea5088b15
Alexandra Esteves <leopoldoalexandra@gmail.com>
2024-10-11 13:19:02 -0300

    subindo projeto

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./FooterStyle.scss";
+
+function Footer() {
+    return(
+        <footer>
+           <div>
+            <h2>Todos os direitos reservados</h2>
+           </div>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import "./MainStyle.scss";
+
+function Main() {
+    return(
+        <main>
+           <h1>Vai na Flix</h1>
+           <section>
+                <figure>
+                    <img src="https://www.justwatch.com/images/poster/176980279/s718/diarios-de-um-vampiro.jpg" alt="" />
+                    <img src="https://www.constancezahn.com/wp-content/uploads/2022/04/foto-capa-serie-dorama-pousando-no-amor.jpg" alt="" />
+                    <img src="" alt="" />
+                </figure>
+           </section>
+        
+        </main>
+    ) 
+}
+export default Main
