ed9fd81e76500a1a45280564a242c86dd8d23a9e
Alexandra Esteves <leopoldoalexandra@gmail.com>
2024-10-11 15:22:46 -0300

    Subindo alterações do projeto

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,13 +3,22 @@ import "./HeaderStyle.scss";
 function Header() {
     return(
        <header>
+            <div className="logo-nav">
+            <img src="https://ampost.com.br/wp-content/uploads/2024/04/melhores-series-da-netflix-para-maratonar.jpg" alt="Netflix Logo" />
             <nav>
                <ul>
-                 <li>Sobre</li>
-                 <li>Contato</li>
-                 <li>Saiba mais</li>
+                 <li>Início</li>
+                 <li>Séries</li>
+                 <li>Filmes</li>
+                 <li>Mais Recentes</li>
+                 <li>Minha Lista</li>
                </ul>
             </nav>
+            </div>
+            <div className="trocar-perfil">
+                <a href="#"><p>Infantil</p></a>
+                <a href="#"><img src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg" alt="" /></a>
+            </div>
        </header>
     )
 }
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,14 +3,24 @@ import "./MainStyle.scss";
 function Main() {
     return(
         <main>
-           <h1>Vai na Flix</h1>
-           <section>
-                <figure>
-                    <img src="https://www.justwatch.com/images/poster/176980279/s718/diarios-de-um-vampiro.jpg" alt="" />
-                    <img src="https://www.constancezahn.com/wp-content/uploads/2022/04/foto-capa-serie-dorama-pousando-no-amor.jpg" alt="" />
-                    <img src="" alt="" />
-                </figure>
+           
+           <section className="inicio-content">
+            <div className="content-capa">
+            
+            </div>
            </section>
+           <section className="series-cartaz-content">
+            <h2>Séries</h2>
+                <ul>
+                    <li><img src="https://www.justwatch.com/images/poster/176980279/s718/diarios-de-um-vampiro.jpg" alt="" /></li>
+                    <li><img src="https://media.fstatic.com/mBm6qmp5WIBqCiJzen2Eapt1ix0=/322x478/smart/filters:format(webp)/media/movies/covers/2013/05/97f855b4918d62d6d9c9f5f94521faa3.jpg" alt="" /></li>
+                    <li><img src="https://uploads.jovemnerd.com.br/wp-content/uploads/2024/03/poster_rainha_das_lagrimas_netflix__32300vc5s-760x1126.jpg" alt="" /></li>
+                    <li><img src="https://www.papodecinema.com.br/wp-content/uploads/2022/08/20220804-uma-advogada-extraordinaria-papo-de-cinema-cartaz-teaser.png" alt="" /></li>
+                    <li><img src="https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg" alt="" /></li>
+                    <li><img src="https://i.ebayimg.com/images/g/sZAAAOSwi6BlBO1z/s-l1200.jpg" alt="" /></li>
+                </ul>
+           </section>
+           <section className="filmes-cartaz-content"></section>
         
         </main>
     ) 
