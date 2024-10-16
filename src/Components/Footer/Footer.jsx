59d8539c667059c5a328f4b79e609f8523c46520
Alexandra Esteves <leopoldoalexandra@gmail.com>
2024-10-16 20:19:55 -0300

    refactory: atualizações

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,8 +3,17 @@ import "./FooterStyle.scss";
 function Footer() {
     return(
         <footer>
-           <div>
-            <h2>Todos os direitos reservados</h2>
+           <div className="rodape-contact-help">
+            <ul>
+                <li><a href="https://help.netflix.com/pt/contactus">Contato</a></li>
+                <li><a href="https://help.netflix.com/pt">Ajuda</a></li>
+            </ul>
+           </div>
+           <div className="redes-sociais">
+            <ul>
+                <li><a href="https://www.instagram.com/netflixbrasil/" target="blank"><img src="https://i.pinimg.com/originals/8a/3b/05/8a3b055d7e3cc52a0f0053535f0339ea.jpg" alt="" /></a></li>
+                <li></li>
+            </ul>
            </div>
         </footer>
     )
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,8 @@
+
 import "./MainStyle.scss";
 
 function Main() {
+
     return(
         <main>
            
@@ -18,9 +20,25 @@ function Main() {
                     <li><img src="https://www.papodecinema.com.br/wp-content/uploads/2022/08/20220804-uma-advogada-extraordinaria-papo-de-cinema-cartaz-teaser.png" alt="" /></li>
                     <li><img src="https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg" alt="" /></li>
                     <li><img src="https://i.ebayimg.com/images/g/sZAAAOSwi6BlBO1z/s-l1200.jpg" alt="" /></li>
+                    <li><img src="https://i0.wp.com/image.tmdb.org/t/p/original/z2IFZ1nrzklEiGzz19YoxAwMDrT.jpg?ssl=1" alt="" /></li>
+                    <li><img src="https://i.ebayimg.com/00/s/MTYwMFgxMDgw/z/mFcAAOSwKoNj5GfS/$_57.JPG?set_id=880000500F" alt="" /></li>
+                    <li><img src="https://www.geekmagazine.com.br/wp-content/uploads/2022/03/290c0490-d436-409e-8ebb-4fad84fe7494-70783-00000f63c91bb8d0_file.webp" alt="" /></li>
                 </ul>
            </section>
-           <section className="filmes-cartaz-content"></section>
+           <section className="filmes-cartaz-content">
+            <h2>Filmes</h2>
+            <ul>
+            <li><img src="https://acdn.mitiendanube.com/stores/004/687/740/products/pos-02108-ef52f1a5d9c36b5a8d17181246905218-1024-1024.jpg" alt="" /></li>
+            <li><img src="https://ae01.alicdn.com/kf/H94ac596c5d1c45deaf20a4aa72296294f/Cl-ssico-Filme-Interestelar-Poster-P-steres-Retro-e-Impress-es-Papel-Kraft-Sala-de-Lar.jpg" alt="" /></li>
+            <li><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-divertida-mente-2-b-pop-arte-poster/poparteskins2/pos-03662-60x90cm/116d271182ded19df9cfe8e389091aef.jpeg" alt="" /></li>
+            <li><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-matrix-d-pop-arte-poster/poparteskins2/15938515659/9825152b9ba6382783b506bf7d4b00d9.jpeg" alt="" /></li>
+            <li><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-john-wick-de-volta-ao-jogo-a-pop-arte-poster/poparteskins2/15938533127/532623d381723db1d5c8a491b0436a2c.jpeg" alt="" /></li>
+            <li><img src="https://uauposters.com.br/media/catalog/product/4/7/470020230615-uau-posters-seven-os-sete-crimes-capitais-classico-filme-5.jpg" alt="" /></li>
+            <li><img src="https://i.ebayimg.com/images/g/BaUAAOSwakdhQLZw/s-l1200.jpg" alt="" /></li>
+            <li><img src="https://br.web.img3.acsta.net/pictures/19/02/21/10/43/0032532.jpg" alt="" /></li>
+            </ul>
+
+           </section>
         
         </main>
     ) 
