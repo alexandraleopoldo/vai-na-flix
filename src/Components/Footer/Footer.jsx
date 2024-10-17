5ba37166806317888ade21f338e05a5da4d3dc5d
Alexandra Esteves <leopoldoalexandra@gmail.com>
2024-10-17 11:51:19 -0300

    refactory: responsividade

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,6 +5,8 @@ function Footer() {
         <footer>
            <div className="rodape-contact-help">
             <ul>
+           
+                <li className="ocultar">Todos os direitos reservados</li>
                 <li><a href="https://help.netflix.com/pt/contactus">Contato</a></li>
                 <li><a href="https://help.netflix.com/pt">Ajuda</a></li>
             </ul>
@@ -12,7 +14,7 @@ function Footer() {
            <div className="redes-sociais">
             <ul>
                 <li><a href="https://www.instagram.com/netflixbrasil/" target="blank"><img src="https://i.pinimg.com/originals/8a/3b/05/8a3b055d7e3cc52a0f0053535f0339ea.jpg" alt="" /></a></li>
-                <li></li>
+                <li><a href="https://www.facebook.com/netflixbrasil/?locale=pt_BR"><img src="https://i.pinimg.com/736x/f1/ef/e0/f1efe080194b6d5e2843f3118634551e.jpg" alt="" /></a></li>
             </ul>
            </div>
         </footer>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,19 +4,20 @@ function Header() {
     return(
        <header>
             <div className="logo-nav">
-            <img src="https://ampost.com.br/wp-content/uploads/2024/04/melhores-series-da-netflix-para-maratonar.jpg" alt="Netflix Logo" />
+            <img src="./img/aleflix.png" alt="Netflix Logo" />
             <nav>
                <ul>
                  <li>Início</li>
-                 <li>Séries</li>
-                 <li>Filmes</li>
+                 <li className="ocultar">Séries</li>
+                 <li className="ocultar">Filmes</li>
                  <li>Mais Recentes</li>
                  <li>Minha Lista</li>
+                 <li className="ocultar">Infantil</li>
                </ul>
             </nav>
             </div>
             <div className="trocar-perfil">
-                <a href="#"><p>Infantil</p></a>
+                
                 <a href="#"><img src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg" alt="" /></a>
             </div>
        </header>
