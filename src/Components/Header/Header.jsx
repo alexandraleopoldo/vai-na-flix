24aa6ed3f2abf0af9a898d9bc4807f210392183f
Alexandra Esteves <leopoldoalexandra@gmail.com>
2024-10-21 22:11:22 -0300

    modificando alguns estilos

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,21 +4,20 @@ function Header() {
     return(
        <header>
             <div className="logo-nav">
-            <img src="./img/aleflix.png" alt="Netflix Logo" />
-            <nav>
-               <ul>
-                 <li>Início</li>
-                 <li className="ocultar">Séries</li>
-                 <li className="ocultar">Filmes</li>
-                 <li>Mais Recentes</li>
-                 <li>Minha Lista</li>
-                 <li className="ocultar">Infantil</li>
-               </ul>
-            </nav>
+              <div className="img-logo"><img src="./img/aleflix.png" alt="Netflix Logo" /></div>
+                <nav>
+                    <ul>
+                      <li >Início</li>
+                      <li >Séries</li>
+                      <li >Filmes</li>
+                      <li >Mais Recentes</li>
+                      <li >Minha Lista</li>
+                      <li >Infantil</li>
+                    </ul>
+              </nav>
             </div>
             <div className="trocar-perfil">
-                
-                <a href="#"><img src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg" alt="" /></a>
+              <a href="#"><img src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg" alt="" /></a>
             </div>
        </header>
     )
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -14,28 +14,28 @@ function Main() {
            <section className="series-cartaz-content">
             <h2>Séries</h2>
                 <ul>
-                    <li><a href="https://www.youtube.com/watch?v=BmVmhjjkN4E" target="blank"><img src="https://www.justwatch.com/images/poster/176980279/s718/diarios-de-um-vampiro.jpg" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=GXrDYboUnnw" target="blank"><img src="https://media.fstatic.com/mBm6qmp5WIBqCiJzen2Eapt1ix0=/322x478/smart/filters:format(webp)/media/movies/covers/2013/05/97f855b4918d62d6d9c9f5f94521faa3.jpg" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=_rNtXO6JuaQ" target="blank"><img src="https://uploads.jovemnerd.com.br/wp-content/uploads/2024/03/poster_rainha_das_lagrimas_netflix__32300vc5s-760x1126.jpg" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=9SdYFYflVbw&t=2s" target="blank"><img src="https://www.papodecinema.com.br/wp-content/uploads/2022/08/20220804-uma-advogada-extraordinaria-papo-de-cinema-cartaz-teaser.png" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=2-W6_6gJda0" target="blank"><img src="https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=YQeUmSD1c3g" target="blank"><img src="https://i.ebayimg.com/images/g/sZAAAOSwi6BlBO1z/s-l1200.jpg" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=Es3B24z8fdA" target="blank"><img src="https://i0.wp.com/image.tmdb.org/t/p/original/z2IFZ1nrzklEiGzz19YoxAwMDrT.jpg?ssl=1" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=uLtkt8BonwM" target="blank"><img src="https://i.ebayimg.com/00/s/MTYwMFgxMDgw/z/mFcAAOSwKoNj5GfS/$_57.JPG?set_id=880000500F" alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/watch?v=x7Krla_UxRg" target="blank"><img src="https://www.geekmagazine.com.br/wp-content/uploads/2022/03/290c0490-d436-409e-8ebb-4fad84fe7494-70783-00000f63c91bb8d0_file.webp" alt="" /></a></li>
+                    <li><a href="https://www.primevideo.com/detail/0HG2QOPONEONZPP2ZXNMA6M8Q7/ref=atv_sr_fle_c_Tn74RA_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B07CNF2JQW&qid=1729553980599" target="blank"><img src="https://www.justwatch.com/images/poster/176980279/s718/diarios-de-um-vampiro.jpg" alt="Diarios De Um Vampiro" /></a></li>
+                    <li><a href="https://www.primevideo.com/detail/0PAM7NQX6QZ4R9AXS0HSZHNYR7/ref=atv_sr_fle_c_Tn74RA_2_1_2?sr=1-2&pageTypeIdSource=ASIN&pageTypeId=B0D1VX5SKJ&qid=1729553980599" target="blank"><img src="https://media.fstatic.com/mBm6qmp5WIBqCiJzen2Eapt1ix0=/322x478/smart/filters:format(webp)/media/movies/covers/2013/05/97f855b4918d62d6d9c9f5f94521faa3.jpg" alt="Os Originais" /></a></li>
+                    <li><a href="https://www.netflix.com/br/title/81707950?source=35" target="blank"><img src="https://uploads.jovemnerd.com.br/wp-content/uploads/2024/03/poster_rainha_das_lagrimas_netflix__32300vc5s-760x1126.jpg" alt="Rainha Das Lágrimas" /></a></li>
+                    <li><a href="https://www.netflix.com/br/title/81518991?source=35" target="blank"><img src="https://www.papodecinema.com.br/wp-content/uploads/2022/08/20220804-uma-advogada-extraordinaria-papo-de-cinema-cartaz-teaser.png" alt="Uma Advogada Extraordinária" /></a></li>
+                    <li><a href="https://www.netflix.com/br/title/70143836?source=35" target="blank"><img src="https://br.web.img3.acsta.net/pictures/14/03/31/19/28/462555.jpg" alt="Breaking Bad" /></a></li>
+                    <li><a href="https://www.primevideo.com/detail/0MZFO9CZW2OOYPKLWW8P6NKD0O/ref=atv_sr_fle_c_Tn74RA_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B09H68V27K&qid=1729553877940" target="blank"><img src="https://i.ebayimg.com/images/g/sZAAAOSwi6BlBO1z/s-l1200.jpg" alt="Dexter" /></a></li>
+                    <li><a href="https://www.primevideo.com/dp/amzn1.dv.gti.44685577-b683-4ae8-8e2c-8e8b1b80a4ed?autoplay=0&ref_=atv_cf_strg_wb" target="blank"><img src="https://i0.wp.com/image.tmdb.org/t/p/original/z2IFZ1nrzklEiGzz19YoxAwMDrT.jpg?ssl=1" alt="Hannibal" /></a></li>
+                    <li><a href="https://www.max.com/br/pt/shows/last-of-us/93ba22b1-833e-47ba-ae94-8ee7b9eefa9a?utm_source=universal_search" target="blank"><img src="https://i.ebayimg.com/00/s/MTYwMFgxMDgw/z/mFcAAOSwKoNj5GfS/$_57.JPG?set_id=880000500F" alt="The Last Of Us" /></a></li>
+                    <li><a href="https://www.disneyplus.com/pt-br/series/moon-knight/4S3oOF1knocS" target="blank"><img src="https://www.geekmagazine.com.br/wp-content/uploads/2022/03/290c0490-d436-409e-8ebb-4fad84fe7494-70783-00000f63c91bb8d0_file.webp" alt="Moon Knight" /></a></li>
                 </ul>
            </section>
            <section className="filmes-cartaz-content">
             <h2>Filmes</h2>
             <ul>
-            <li><img src="https://acdn.mitiendanube.com/stores/004/687/740/products/pos-02108-ef52f1a5d9c36b5a8d17181246905218-1024-1024.jpg" alt="" /></li>
-            <li><img src="https://ae01.alicdn.com/kf/H94ac596c5d1c45deaf20a4aa72296294f/Cl-ssico-Filme-Interestelar-Poster-P-steres-Retro-e-Impress-es-Papel-Kraft-Sala-de-Lar.jpg" alt="" /></li>
-            <li><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-divertida-mente-2-b-pop-arte-poster/poparteskins2/pos-03662-60x90cm/116d271182ded19df9cfe8e389091aef.jpeg" alt="" /></li>
-            <li><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-matrix-d-pop-arte-poster/poparteskins2/15938515659/9825152b9ba6382783b506bf7d4b00d9.jpeg" alt="" /></li>
-            <li><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-john-wick-de-volta-ao-jogo-a-pop-arte-poster/poparteskins2/15938533127/532623d381723db1d5c8a491b0436a2c.jpeg" alt="" /></li>
-            <li><img src="https://uauposters.com.br/media/catalog/product/4/7/470020230615-uau-posters-seven-os-sete-crimes-capitais-classico-filme-5.jpg" alt="" /></li>
-            <li><img src="https://i.ebayimg.com/images/g/BaUAAOSwakdhQLZw/s-l1200.jpg" alt="" /></li>
-            <li><img src="https://br.web.img3.acsta.net/pictures/19/02/21/10/43/0032532.jpg" alt="" /></li>
+            <li><a href="https://www.max.com/br/pt/movies/batman-o-cavaleiro-das-trevas-ressurge/6ce5965d-cdb2-4f9c-b22b-ae7a091d95a8?utm_source=universal_search" target="blank"><img src="https://acdn.mitiendanube.com/stores/004/687/740/products/pos-02108-ef52f1a5d9c36b5a8d17181246905218-1024-1024.jpg" alt="Batman - The Dark Knight Rises" /></a></li>
+            <li><a href="https://www.max.com/br/pt/movies/interestelar/aa5b9295-8f9c-44f5-809b-3f2b84badfbf?utm_source=universal_search" target="blank"><img src="https://ae01.alicdn.com/kf/H94ac596c5d1c45deaf20a4aa72296294f/Cl-ssico-Filme-Interestelar-Poster-P-steres-Retro-e-Impress-es-Papel-Kraft-Sala-de-Lar.jpg" alt="Interestelar" /></a></li>
+            <li><a href="https://www.disney.com.br/filmes/divertidamente-2" target="blank"><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-divertida-mente-2-b-pop-arte-poster/poparteskins2/pos-03662-60x90cm/116d271182ded19df9cfe8e389091aef.jpeg" alt="Divertidamente 2" /></a></li>
+            <li><a href="https://www.max.com/br/pt/movies/matrix/012cacbd-5893-4379-b7a6-d3737c61d4b5?utm_source=universal_search" target="blank"><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-matrix-d-pop-arte-poster/poparteskins2/15938515659/9825152b9ba6382783b506bf7d4b00d9.jpeg" alt="Matrix" /></a></li>
+            <li><a href="https://www.primevideo.com/dp/0QSV36C6S10IZ3NOZILJ71VZ9C?ref_=dvm_pds_tit_br_sw_s_g_mkw_slq0MCmWt-dc&mrntrk=pcrid_690204517141_slid__pgrid_160284736002_pgeo_20094_x__adext__ptid_kwd-983527830&_mkw_slq0MCmWt-dc&mrntrk=pcrid_690204517141_slid__pgrid_160284736002_pgeo_20094_x__adext__ptid_kwd-983527830&gclid=Cj0KCQjw99e4BhDiARIsAISE7P84joYbgD5vSwFY3iSavth5k7JO5FPIwkLZlUQ9-8zlpCqhy4FTL90aAoJOEALw_wcB&gclsrc=aw.ds" target="blank"><img src="https://a-static.mlcdn.com.br/450x450/poster-cartaz-john-wick-de-volta-ao-jogo-a-pop-arte-poster/poparteskins2/15938533127/532623d381723db1d5c8a491b0436a2c.jpeg" alt="John Wick" /></a></li>
+            <li><a href="https://www.netflix.com/pt/title/950149#:~:text=Seven%3A%207%20Pecados%20Mortais%20%7C%20Netflix" target="blank"><img src="https://uauposters.com.br/media/catalog/product/4/7/470020230615-uau-posters-seven-os-sete-crimes-capitais-classico-filme-5.jpg" alt="Seven" /></a></li>
+            <li><a href="https://www.max.com/br/pt/movies/parasita/d5e3be11-eb8b-449f-89cf-db887ddee777?utm_source=universal_search" target="blank"><img src="https://i.ebayimg.com/images/g/BaUAAOSwakdhQLZw/s-l1200.jpg" alt="Parasita" /></a></li>
+            <li><a href="https://www.netflix.com/br/title/80200571?source=35" target="blank"><img src="https://br.web.img3.acsta.net/pictures/19/02/21/10/43/0032532.jpg" alt="Estrada Sem Lei" /></a></li>
             </ul>
 
            </section>
